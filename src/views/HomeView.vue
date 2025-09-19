b48310b0da25a645e168e09a1cef2d7030c1e27c
Isxoqjon <isaakahmedov@gmail.com>
2025-09-19 10:59:40 +0500

    yopildi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -195,17 +195,9 @@ async function handleSVGClick(event) {
       return 1;
     }
 
-    if (sectorID == "1" || sectorID == "1a" || sectorID == "vip" || sectorID == "21" || sectorID == "23") {
-      alert("Bu joylar administrator tomonidan o'chirilgan!")
-      return 1;
-    }
 
-    if (sectorID == "22") {
-      alert("Bu joylar mehmonlar uchun!")
-      return 1;
-    }
-
-    const blockedSectors = ['1', '1a', 'vip', '21', '22', '23', '24', '25', '26', '27', '7', '8', '9', '10', '11', '12', '13'];
+    // const blockedSectors = ['1', '1a', 'vip', '21', '22', '23', '24', '25', '26', '27', '7', '8', '9', '10', '11', '12', '13'];
+    const blockedSectors = ['21', '22', '23'];
 
     const normalizedSectorID = sectorID.toLowerCase();
 
